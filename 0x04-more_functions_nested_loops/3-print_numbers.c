9c34dcdca7f418f19c9b458cc2229ffa47edffb8
Peter Kariuki <pkariukikimani383@gmail.com>
2023-06-23 09:20:06 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: numbers from 0 to 9
+ */
+void print_numbers(void)
+
+{
+	int z;
+
+	for (z = 0; z <= 9; z++)
+	{
+		_putchar(z + '0');
+	}
+	_putchar('\n');
+}
