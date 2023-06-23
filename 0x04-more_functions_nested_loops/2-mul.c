3d6beddbf36244d971eb8521adab6213d63845b9
Peter Kariuki <pkariukikimani383@gmail.com>
2023-06-23 09:13:10 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @y: first number to multiply
+ * @z: second number to multiply
+ * Return: result of multiplication between y and z
+ */
+int mul(int y, int z)
+{
+	return (y * z);
+}
