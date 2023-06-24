6d85d3103b392d677e89c9d6f4d0c82defd189ee
Peter Kariuki <pkariukikimani383@gmail.com>
2023-06-23 19:33:36 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of squares
+ * @size: size of the squares triangle
+ * Return: Empty
+ */
+
+void print_triangle(int size)
+{
+	int n, o, p;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (n = 0; n < size; n++)
+	{
+	for (o = size - n; o > 0; o--)
+	{
+		_putchar(32);
+	}
+	for (p = 0; p <= n; p++)
+	{
+		_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
