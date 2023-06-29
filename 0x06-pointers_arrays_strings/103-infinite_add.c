9bcfa8abf13e0ad9c4351bdaf8e4ffa9c9159d67
Peter Kariuki <pkariukikimani383@gmail.com>
2023-06-29 16:14:11 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,7 +12,7 @@ void rev_string(char *n)
 	int j = 0;
 	char temp;
 
-	while (*(n + 1) != '\0')
+	while (*(n + i) != '\0')
 	{
 		i++;
 	}
@@ -28,8 +28,8 @@ void rev_string(char *n)
 
 /**
  * infinite_add - add 2 numbers together
- * @n1: first number
- * @n2: second number
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
  * @size_r: buffer size
  * Return: pointer to calling function
