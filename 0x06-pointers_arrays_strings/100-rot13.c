f7a11769dc230778053a1c266c67ce6e8101e3ca
Peter Kariuki <pkariukikimani383@gmail.com>
2023-06-29 14:54:49 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int b;
+	int c;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (b = 0; s[b] != '\0'; b++)
+	{
+		for (c = 0; c < 52; c++)
+		{
+			if (s[b] == data1[c])
+			{
+				s[b] = datarot[c];
+				break;
+			}
+		}
+	}
+	return (s);
+}
